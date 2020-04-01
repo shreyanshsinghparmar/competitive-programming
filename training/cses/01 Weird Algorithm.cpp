eb68ac6866421aa7e888a3bb7bb3d8ada40511b9
Shreyansh Singh <45299337+iamshr3y@users.noreply.github.com>
2020-04-01 14:05:23 +0530

    add first cses problem

---
+++ training/cses/01 Weird Algorithm.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+ 
+#define int long long
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+ 
+using namespace std;
+ 
+int32_t main() {
+    fastio;
+ 
+    int n;
+    cin >> n;
+    while(n != 1) {
+		cout << n << " ";
+		if(n % 2 != 0) {
+			n = n * 3 + 1;
+		} else {
+			n /= 2;
+		}
+	}
+	cout << 1;
+	cout << "\n";
+ 
+    return 0;
+}

6048df20185946f25691b71a49f317d8212b6aac
Shreyansh Singh <45299337+iamshr3y@users.noreply.github.com>
2020-04-28 17:43:49 +0530

    codeforces string task

---
+++ A2OJ/36_String_Task.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+ 
+using namespace std;
+
+int32_t main() {
+	fastio;
+
+	string s;
+	cin >> s;
+	for(char c : s) {
+		char curr = tolower(c);
+		if(curr == 'a' || curr == 'e' || curr == 'i' || curr == 'o' || curr == 'u' || curr == 'y') continue;
+		else cout << '.' << curr;
+	}
+
+	return 0;
+}
+
+/*
+A. String Task
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Petya started to attend programming lessons. On the first lesson his task was to write a simple program. The program was supposed to do the following: in the given string, consisting if uppercase and lowercase Latin letters, it:
+
+deletes all the vowels,
+inserts a character "." before each consonant,
+replaces all uppercase consonants with corresponding lowercase ones.
+Vowels are letters "A", "O", "Y", "E", "U", "I", and the rest are consonants. The program's input is exactly one string, it should return the output as a single string, resulting after the program's processing the initial string.
+
+Help Petya cope with this easy task.
+
+Input
+The first line represents input string of Petya's program. This string only consists of uppercase and lowercase Latin letters and its length is from 1 to 100, inclusive.
+
+Output
+Print the resulting string. It is guaranteed that this string is not empty.
+
+Examples
+inputCopy
+tour
+outputCopy
+.t.r
+inputCopy
+Codeforces
+outputCopy
+.c.d.f.r.c.s
+inputCopy
+aBAcAba
+outputCopy
+.b.c.b
+*/

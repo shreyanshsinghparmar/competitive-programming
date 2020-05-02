ac28b6bec1d0a997f0a226a3235e9e2be3560e36
Shreyansh Singh <45299337+iamshr3y@users.noreply.github.com>
2020-05-02 18:21:22 +0530

    codechef atm

---
+++ random/05_ATM.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+ 
+using namespace std;
+
+const unsigned int M = 1e9 + 7;
+
+int32_t main() {
+	fastio;
+
+	int x;
+	double y;
+	cin >> x >> y;
+	if(x % 5 == 0 && x <= y - 0.50) {
+		y -= (double) x;
+		y -= 0.50;
+	}
+	printf("%0.2f", y);
+
+	return 0;
+}
+
+/*
+ATM
+Pooja would like to withdraw X $US from an ATM. The cash machine will only accept the transaction if X is a multiple of 5, and Pooja's account balance has enough cash to perform the withdrawal transaction (including bank charges). For each successful withdrawal the bank charges 0.50 $US. Calculate Pooja's account balance after an attempted transaction.
+
+Input
+Positive integer 0 < X <= 2000 - the amount of cash which Pooja wishes to withdraw.
+
+Nonnegative number 0<= Y <= 2000 with two digits of precision - Pooja's initial account balance.
+
+Output
+Output the account balance after the attempted transaction, given as a number with two digits of precision. If there is not enough money in the account to complete the transaction, output the current bank balance.
+
+Example - Successful Transaction
+Input:
+30 120.00
+
+Output:
+89.50
+Example - Incorrect Withdrawal Amount (not multiple of 5)
+Input:
+42 120.00
+
+Output:
+120.00
+Example - Insufficient Funds
+Input:
+300 120.00
+
+Output:
+120.00
+*/

9f967f65dcbf41d2cab9d3d754fa2ea3f975244b
Shreyansh Singh <45299337+iamshr3y@users.noreply.github.com>
2020-05-02 18:27:20 +0530

    codechef turbo sort

---
+++ random/06_Turbo_Sort.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+ 
+using namespace std;
+
+const unsigned int M = 1e9 + 7;
+
+int32_t main() {
+	fastio;
+
+	int n;
+	cin >> n;
+	int a[n];
+	for(int i = 0; i < n; ++i) cin >> a[i];
+	sort(a, a + n);
+	for(int i : a) cout << i << '\n';
+	return 0;
+}
+
+/*
+Turbo Sort
+Given the list of numbers, you are to sort them in non decreasing order.
+Input
+t – the number of numbers in list, then t lines follow [t <= 10^6].
+Each line contains one integer: N [0 <= N <= 10^6]
+
+Output
+Output given numbers in non decreasing order.
+Example
+Input:
+
+5
+5
+3
+6
+7
+1
+Output:
+
+1
+3
+5
+6
+7
+*/

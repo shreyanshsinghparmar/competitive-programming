d1797462cba87509b5155a5a9e5422a4676aa931
Shreyansh Singh <45299337+iamshr3y@users.noreply.github.com>
2020-05-04 12:55:41 +0530

    codeforces watermelon

---
+++ random/30_Watermelon.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+ 
+using namespace std;
+
+const unsigned int M = 1e9 + 7;
+
+int32_t main() {
+	fastio;
+
+	int n;
+	cin >> n;
+	if(n == 2) cout << "NO", exit(0);
+	cout << (n % 2 == 0 ? "YES" : "NO");
+
+	return 0;
+}
+
+/*
+A. Watermelon
+time limit per test1 second
+memory limit per test64 megabytes
+inputstandard input
+outputstandard output
+One hot summer day Pete and his friend Billy decided to buy a watermelon. They chose the biggest and the ripest one, in their opinion. After that the watermelon was weighed, and the scales showed w kilos. They rushed home, dying of thirst, and decided to divide the berry, however they faced a hard problem.
+
+Pete and Billy are great fans of even numbers, that's why they want to divide the watermelon in such a way that each of the two parts weighs even number of kilos, at the same time it is not obligatory that the parts are equal. The boys are extremely tired and want to start their meal as soon as possible, that's why you should help them and find out, if they can divide the watermelon in the way they want. For sure, each of them should get a part of positive weight.
+
+Input
+The first (and the only) input line contains integer number w (1 ≤ w ≤ 100) — the weight of the watermelon bought by the boys.
+
+Output
+Print YES, if the boys can divide the watermelon into two parts, each of them weighing even number of kilos; and NO in the opposite case.
+
+Examples
+inputCopy
+8
+outputCopy
+YES
+Note
+For example, the boys can divide the watermelon into two parts of 2 and 6 kilos respectively (another variant — two parts of 4 and 4 kilos).
+*/

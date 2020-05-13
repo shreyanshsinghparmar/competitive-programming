b2aa7bdbaffb86d3fc4ae6e73400f52bc2f45b2e
Shreyansh Singh <45299337+iamshr3y@users.noreply.github.com>
2020-05-13 07:13:57 +0530

    codeforces soldiers and bananas

---
+++ random/79_Soldiers_and_Bananas.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+ 
+using namespace std;
+
+const unsigned int M = 1e9 + 7;
+
+int32_t main() {
+	fastio;
+
+	int k, n, w;
+	cin >> k >> n >> w;
+	int cost = 0;
+	for(int i = 1; i <= w; ++i) cost += i * k;
+	int borrow = cost - n;
+	cout << (borrow > 0 ? borrow : 0);
+
+	return 0;
+}
+
+/*
+A. Soldier and Bananas
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+A soldier wants to buy w bananas in the shop. He has to pay k dollars for the first banana, 2k dollars for the second one and so on (in other words, he has to pay i·k dollars for the i-th banana).
+
+He has n dollars. How many dollars does he have to borrow from his friend soldier to buy w bananas?
+
+Input
+The first line contains three positive integers k, n, w (1  ≤  k, w  ≤  1000, 0 ≤ n ≤ 109), the cost of the first banana, initial number of dollars the soldier has and number of bananas he wants.
+
+Output
+Output one integer — the amount of dollars that the soldier must borrow from his friend. If he doesn't have to borrow money, output 0.
+
+Examples
+inputCopy
+3 17 4
+outputCopy
+13
+*/
